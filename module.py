9281b23cf321180cd5d548426be6ff679cd4798a
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-07-07 13:29:29 +0700

    Add files via upload

--- module.py
+++ module.py
@@ -19,35 +19,15 @@ import ast
 import tkinter.simpledialog
 
 def load_all_model():
-    global MODEL_MEAN_VALUES,ageList,genderList,faceNet,ageNet,genderNet,model
+    global model
+    model_conf, model_iou = read_nvr(7)
     print('start load model!!!')
     model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
-    model.conf = 0.2
-    model.iou = 0.4
+    model.conf = model_conf
+    model.iou = model_iou
     model.classes = [0]
     model.amp = True
     print('load yolov5 successfully!!!')
-
-    print('load gender & age model')
-    if os.path.isdir('gender_age_model') == False:
-        url = "https://drive.google.com/drive/u/1/folders/1n7WSJV0CdGY8vaPukLxZDXT3TjzEZ-Hp"
-        gdown.download_folder(url)
-
-    faceProto="gender_age_model/opencv_face_detector.pbtxt"
-    faceModel="gender_age_model/opencv_face_detector_uint8.pb"
-    ageProto="gender_age_model/age_deploy.prototxt"
-    ageModel="gender_age_model/age_net.caffemodel"
-    genderProto="gender_age_model/gender_deploy.prototxt"
-    genderModel="gender_age_model/gender_net.caffemodel"
-
-    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-    ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-    genderList=['Male','Female']
-
-    faceNet=cv2.dnn.readNet(faceModel,faceProto)
-    ageNet=cv2.dnn.readNet(ageModel,ageProto)
-    genderNet=cv2.dnn.readNet(genderModel,genderProto)
-    print('load gender & age successfully!!!')
 
 def cam_threading(rtsp_url,num):
     cap = cv2.VideoCapture(rtsp_url)
@@ -69,31 +49,27 @@ def get_rtsp(rtsp_url,num,st=None):
             print('auto start!!!')
             os.execv(sys.executable, ['python'] + sys.argv)
             break
+
         frame = cv2.resize(frame, (640, 360))
+        Date = datetime.datetime.now().strftime("%d/%m/%Y")
+        Time = datetime.datetime.now().strftime("%T")
+        file_name = Time.replace(':', '-')
+        b = str(file_name)
         if st == None:
            st = time.time()
         et = time.time()
         time_ref = read_nvr(4)
         if et - st > int(time_ref):
-            save_img = read_nvr(3)
             polygon_employ, polygon_customer = read_polygon_value(rtsp_url)
             if polygon_employ != False:
                 if type(polygon_employ) == str:
                     polygon_employ = ast.literal_eval(polygon_employ)
                     polygon_customer = ast.literal_eval(polygon_customer)
-                Date = datetime.datetime.now().strftime("%d/%m/%Y")
-                Time = datetime.datetime.now().strftime("%T")
 
-                file_name = Time.replace(':', '-')
-                file_name = num + '_' + file_name
-                date_img = build_folder_file()
-                img_file = date_img + '/' + file_name + '.jpg'
-                if save_img == 'TRUE':
-                    cv2.imwrite(img_file, frame)
-                output_flask_process, output_flask_process_gender_age,check_break = request_post_onprocess(num, frame, Date, Time,
-                                                                                               file_name,
-                                                                                               polygon_customer,
-                                                                                               polygon_employ, model)
+                output_flask_process,check_break = request_post_onprocess(num, frame, Date, Time,
+                                                                           file_name,
+                                                                           polygon_customer,
+                                                                           polygon_employ, model)
                 # print(output_flask_process, output_flask_process_gender_age)
                 st = time.time()
                 if check_break == True:
@@ -101,6 +77,13 @@ def get_rtsp(rtsp_url,num,st=None):
             else:
                 print('No polygon stop {}'.format(rtsp_url))
                 break
+
+        cv2.imshow('{}'.format(num),frame)
+        k = cv2.waitKey(1)
+        if k == ord('q') or b > '21:00:00':
+            break
+    cap.release()
+    cv2.destroyWindow('{}'.format(num))
 
 def build_folder_file():
     base_dir = pathlib.Path(__file__).parent.absolute()
@@ -111,49 +94,26 @@ def build_folder_file():
         os.mkdir(backup_img)
     if os.path.isdir(f'{date_img}') == False:
         os.mkdir(date_img)
-    # try:
-    #     with open('backup_file/Head-count(not for open).csv') as f:
-    #         pass
-    # except:
-    #     header = ['device_name','File name', 'วัน', 'เวลา', 'จำนวนคนทั้งหมด', 'พนักงาน advice', 'ลูกค้า', 'จำนวนคนที่เดินผ่าน',
-    #               'POST STATUS']
-    #     with open('backup_file/Head-count(not for open).csv', 'w', encoding='UTF-8-sig', newline='') as f:
-    #         writer = csv.writer(f)
-    #         writer.writerow(header)
 
     return date_img
 
 def request_post_onprocess(device_name,frame,date,time,file_name, polygon_nodetect, polygon_employ, model):
     camera_opened = read_nvr(2)
+    save_img = read_nvr(3)
+    auto_start = read_nvr(6)
     employee = 0
     customer = 0
     walking_pass = 0
-    gender_array = []
-    age_array = []
-    count_men = 0
-    count_women = 0
-    count0_2 = 0
-    count4_6 = 0
-    count8_12 = 0
-    count15_20 = 0
-    count25_32 = 0
-    count38_43 = 0
-    count48_53 = 0
-    count60_100 = 0
-    output = []
     check_break = False
 
-    url = 'https://globalapi.advice.co.th/api/upload_people'
+    url = 'https://mltest.advice.co.th/mltest/post_diy_commart.php'
     # url = None
 
-    # try:
-    #     repost_logfile(url)
-    # except:
-    #     pass
-    frame_gender = frame.copy()
+    frame_raw = frame.copy()
     results = model(frame, size=640)
+    if auto_start == 'FALSE':
+        results.show()
     out2 = results.pandas().xyxy[0]
-
     if len(out2) != 0:
         for i in range(len(out2)):
             xmin = int(out2.iat[i, 0])
@@ -163,11 +123,18 @@ def request_post_onprocess(device_name,frame,date,time,file_name, polygon_nodete
 
             cenx = (xmax + xmin) // 2
             ceny = (ymax + ymin) // 2
+            conf = out2.iat[i, 4]
             obj_name = out2.iat[i, 6]
             if obj_name == 'person' or obj_name == '0':
 
-                frame_face = frame_gender[ymin:ymax, xmin:xmax]
-                # gender_array,age_array = gender_age(frame_face,faceNet,ageNet,genderNet,gender_array,age_array)
+                if save_img == 'TRUE':
+                    frame_to_save = frame_raw[ymin:ymax,xmin:xmax]
+                    file_name = device_name + '_' + file_name
+                    date_img = build_folder_file()
+                    img_file = date_img + '/' + file_name + '.jpg'
+                    if len(os.listdir(date_img)) < 10000:
+                        if conf > 0.7:
+                            cv2.imwrite(img_file, frame_to_save)
 
                 color = draw_polygon(cenx, ceny, polygon_nodetect, polygon_employ)
 
@@ -180,45 +147,41 @@ def request_post_onprocess(device_name,frame,date,time,file_name, polygon_nodete
                 elif color == (0, 0, 0):
                     pass
 
-        count_men = gender_array.count('Male')
-        count_women = gender_array.count('Female')
-
-        count0_2 = age_array.count('(0-2)')
-        count4_6 = age_array.count('(4-6)')
-        count8_12 = age_array.count('(8-12)')
-        count15_20 = age_array.count('(15-20)')
-        count25_32 = age_array.count('(25-32)')
-        count38_43 = age_array.count('(38-43)')
-        count48_53 = age_array.count('(48-53)')
-        count60_100 = age_array.count('(60-100)')
-
     count_all_json = employee+customer+walking_pass
     dd, mm, yyyy = date.split('/')
     date_json = f'{yyyy}-{mm}-{dd}'
     time_json = date_json + f' {time}'
 
-    output_flask_process = {"people_device": device_name,"img_name": file_name, "img_date": date_json, "img_time": time_json,
-                            "people_total": count_all_json, "people_advice": employee,
-                            "people_other": customer, "storefront": walking_pass}
-
-    output_flask_process_gender_age = {"count_male": count_men, "count_female": count_women,
-                                       "(0-2)": count0_2,'(4-6)': count4_6,'(8-12)':count8_12,
-                                       '(15-20)': count15_20,'(25-32)': count25_32,'(38-43)':count38_43,
-                                       '(48-53)': count48_53,'(60-100)': count60_100}
+    # output_flask_process = {"people_device": device_name,"img_name": file_name, "img_date": date_json, "img_time": time_json,
+    #                         "people_total": count_all_json, "people_advice": employee,
+    #                         "people_other": customer, "storefront": walking_pass}
+    # output_flask_process = {'branch_cuscode5': 'KS513', 'camera_id': device_name, 'cus_group': 'walk pass',
+    #                         'gender': 'emp', 'count_person': employee, 'token': 'dkjfkdsjskfa'}
+    output_flask_process = {'branch_cuscode5': device_name, 'camera_id': device_name, 'cus_group': 'walk pass',
+                            'gender': 'cus', 'count_person': customer, 'token': 'dkjfkdsjskfa'}
+    output_flask_process2 = {'branch_cuscode5': device_name, 'camera_id': device_name, 'cus_group': 'walk pass',
+                            'gender': 'emp', 'count_person': employee, 'token': 'dkjfkdsjskfa'}
 
     text = {"Status_post": 'Addlog'}
     # addlog(device_name, file_name, date_json, time_json, count_all_json, employee, customer, walking_pass)
-    status_post = request_post(url, output_flask_process)
-    if status_post == 0:
-        text['Status_post'] = 'No'
-        print(output_flask_process, text)
+    try:
+        status_post = request_post(url, device_name, employee, customer)
+        if status_post == 0:
+            text['Status_post'] = 'No'
+            print(output_flask_process, text)
+            print(output_flask_process2, text)
+            addlog(device_name, file_name, date_json, time_json, count_all_json, employee, customer, walking_pass)
+        elif status_post == 1:
+            text['Status_post'] = 'Yes'
+            print(output_flask_process, text)
+            print(output_flask_process2, text)
+        elif status_post == 2:
+            text['Status_post'] = 'empty url'
+            print(output_flask_process, text)
+            print(output_flask_process2, text)
+    except Exception as e:
+        print(e)
         addlog(device_name, file_name, date_json, time_json, count_all_json, employee, customer, walking_pass)
-    elif status_post == 1:
-        text['Status_post'] = 'Yes'
-        print(output_flask_process, text)
-    elif status_post == 2:
-        text['Status_post'] = 'empty url'
-        print(output_flask_process, text)
 
     # --------------------------------------------------------
     # status_post_csv = text['Status_post']
@@ -232,7 +195,7 @@ def request_post_onprocess(device_name,frame,date,time,file_name, polygon_nodete
             cv2.destroyWindow(f'{device_name}')
 
 
-    return output_flask_process, output_flask_process_gender_age,check_break
+    return output_flask_process,check_break
 
 def draw_polygon(cenx, ceny, polygon1, polygon2):
     contours1 = np.array(polygon1)
@@ -255,50 +218,6 @@ def draw_polygon(cenx, ceny, polygon1, polygon2):
         color = (0, 0, 0)
     # cv2.imshow("filledPolygon", image)
     return color
-
-def highlightFace(net, frame, conf_threshold=0.7):
-    frameOpencvDnn=frame.copy()
-    frameHeight=frameOpencvDnn.shape[0]
-    frameWidth=frameOpencvDnn.shape[1]
-    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
-    net.setInput(blob)
-    detections=net.forward()
-    faceBoxes=[]
-    for i in range(detections.shape[2]):
-        confidence=detections[0,0,i,2]
-        if confidence>conf_threshold:
-            x1=int(detections[0,0,i,3]*frameWidth)
-            y1=int(detections[0,0,i,4]*frameHeight)
-            x2=int(detections[0,0,i,5]*frameWidth)
-            y2=int(detections[0,0,i,6]*frameHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn,faceBoxes
-
-def gender_age(frame,faceNet,ageNet,genderNet,gender_array,age_array):
-    # padding = 20
-    # resultImg, faceBoxes = highlightFace(faceNet, frame)
-    # if not faceBoxes:
-    #     print("No face detected")
-
-    # for faceBox in faceBoxes:
-    #     face = frame[max(0, faceBox[1] - padding):
-    #                  min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
-    #                                                                 :min(faceBox[2] + padding, frame.shape[1] - 1)]
-
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-    genderNet.setInput(blob)
-    genderPreds = genderNet.forward()
-    gender = genderList[genderPreds[0].argmax()]
-    gender_array.append(gender)
-
-    ageNet.setInput(blob)
-    agePreds = ageNet.forward()
-    age = ageList[agePreds[0].argmax()]
-    age_array.append(age)
-
-    return gender_array,age_array
 
 def create_logfile():
     con = sqlite3.connect('logfile.db')
@@ -341,14 +260,20 @@ def repost_logfile(url):
 
     con.close()
 
-def request_post(url, text):
+def request_post(url, device_name, employee, customer):
     if url == None:
         status_post = 2
     else:
-        response = requests.post(url, json=text)
+        text1 = {'branch_cuscode5': device_name, 'camera_id': device_name, 'cus_group': 'walk pass',
+                 'gender': 'emp', 'count_person': employee, 'token': 'dkjfkdsjskfa'}
+        text2 = {'branch_cuscode5': device_name, 'camera_id': device_name, 'cus_group': 'walk pass',
+                 'gender': 'cus', 'count_person': customer, 'token': 'dkjfkdsjskfa'}
+        response = requests.post(url, data=text1)
+        response2 = requests.post(url, data=text2)
         print('\n------posting------')
         if response.ok:
             print("Upload completed successfully!")
+            # print(response.text)
             status_post = 1
 
         else:
@@ -407,7 +332,10 @@ def set_polygon(rtsp_url):
     check_click = 0
 
     while True:
-        _, img = cap.read()
+        ret, img = cap.read()
+        if ret == False:
+            print('stop set polygon: {}'.format(rtsp_url))
+            break
         img = cv2.resize(img, size_img_vdo)
         if check_click == 2:
             contours1 = np.array(result1)
@@ -492,10 +420,15 @@ def write_nvr():
     write_config.read('config.ini')
     if write_config.has_section('BASE CONFIG') == False:
         write_config.add_section('BASE CONFIG')
-        write_config.set('BASE CONFIG', 'rtsp_source','rtsp://testcam:Password1@advicedvrddns.ddns.net:554/cam/realmonitor?')
+        write_config.add_section('rtsp list')
+        # write_config.set('BASE CONFIG', 'rtsp_source','rtsp://testcam:Password1@advicedvrddns.ddns.net:554/cam/realmonitor?')
+        write_config.set('BASE CONFIG', 'auto start', 'FALSE')
         write_config.set('BASE CONFIG', 'camera opened', 'TRUE')
         write_config.set('BASE CONFIG', 'save image result', 'FALSE')
         write_config.set('BASE CONFIG', 'time ref', '10')
+        write_config.set('BASE CONFIG', 'model conf', '0.5')
+        write_config.set('BASE CONFIG', 'model iou', '0.5')
+        write_config.set('rtsp list', 'rtsp1', '')
         cfgfile = open('config.ini','w')
         write_config.write(cfgfile)
         cfgfile.close()
@@ -515,16 +448,25 @@ def read_nvr(check):
     elif check == 4:
         time_ref = read_config.get('BASE CONFIG', 'time ref')
         return time_ref
+    elif check == 5:
+        rtsp_list_array = []
+        device_name_array = []
+        for (each_key, each_val) in read_config.items('rtsp list'):
+            if each_val != '':
+                rtsp_url,device_name = each_val.split(',')
+                rtsp_list_array.append(rtsp_url)
+                device_name_array.append(device_name)
+        return rtsp_list_array,device_name_array
+    elif check == 6:
+        auto_start = read_config.get('BASE CONFIG', 'auto start')
+        return auto_start
+    elif check == 7:
+        model_conf = read_config.get('BASE CONFIG', 'model conf')
+        model_iou = read_config.get('BASE CONFIG', 'model iou')
+        return model_conf,model_iou
+
 
 def set_polycon_tk(rtsp):
-    # rtsp_source = read_nvr(1)
-    # rtsp_split = rtsp_source.split('/')
-    # if type(num) == int:
-    #     rtsp = num
-    # elif num == 'rtsp://admin:888888@192.168.7.60:10554/tcp/av0_1':
-    #     rtsp = num
-    # else:
-    #     rtsp = '{}//{}/{}/{}'.format(rtsp_split[0],rtsp_split[2],rtsp_split[3],num)
     cap = cv2.VideoCapture(rtsp)
     if cap.isOpened():
         polygon_employ, polygon_customer = set_polygon_zone(rtsp)
--- process.py
+++ process.py
@@ -1,28 +1,15 @@
 from module import *
 
 def run_config():
-    rtsp_split = rtsp_source.split('/', 4)
+    rtsp_list_array,device_name_array = read_nvr(5)
     root = Tk()
     root.title('Edit_polygon')
     root.geometry('250x300+0+0')
 
-    host_name = Label(root, text=rtsp_split[2], fg='green', font=('Arial', 10))
-    host_name.pack(padx=5, pady=5)
-
-    ch1 = Button(root, text="CH1", width=20, bg='red', fg='white', command=lambda
-        cam=rtsp_source + rtsp_url1: set_polycon_tk(cam))
-    ch1.pack(padx=5, pady=5)
-    ch2 = Button(root, text="CH2", width=20, bg='red', fg='white', command=lambda
-        cam=rtsp_source + rtsp_url2: set_polycon_tk(cam))
-    ch2.pack(padx=5, pady=5)
-    ch3 = Button(root, text="CH3", width=20, bg='red', fg='white', command=lambda
-        cam='rtsp://test:advice128@110.49.125.237:554/cam/realmonitor?channel=1&subtype=0': set_polycon_tk(cam))
-    ch3.pack(padx=5, pady=5)
-    ch4 = Button(root, text="CH4", width=20, bg='red', fg='white', command=lambda
-        cam=rtsp_source + rtsp_url4: set_polycon_tk(cam))
-    ch4.pack(padx=5, pady=5)
-
-    # root.attributes('-topmost', True)
+    for i in rtsp_list_array:
+        ch1 = Button(root, text="CH{}".format(rtsp_list_array.index(i)+1), width=20, bg='red', fg='white', command=lambda
+            cam=i: set_polycon_tk(cam))
+        ch1.pack(padx=5, pady=5)
 
     root.mainloop()
 
@@ -32,30 +19,26 @@ def confirm_yesno(message = 'ยืนยันที่จะปิดโปร
         sys.exit(1)
 
 def run_app():
-    cam_threading(rtsp_source + rtsp_url1, 'Commart_channel1')
-    cam_threading(rtsp_source + rtsp_url2, 'Commart_channel2')
-    cam_threading('rtsp://test:advice128@110.49.125.237:554/cam/realmonitor?channel=1&subtype=0', 'Commart_channel3')
-    # cam_threading(rtsp_source + rtsp_url4, 4)
+    rtsp_list_array,device_name_array = read_nvr(5)
+    for j in rtsp_list_array:
+        cam_threading(j,device_name_array[rtsp_list_array.index(j)])
 
 if __name__ == '__main__':
     write_nvr()
-    rtsp_source = read_nvr(1)
-
-    rtsp_url1 = 'channel=14&subtype=0'
-    rtsp_url2 = 'channel=15&subtype=0'
-    rtsp_url3 = 'av0_2'
-    rtsp_url4 = 'av0_3'
+    auto_start = read_nvr(6)
     if os.path.isfile('logfile.db') == False:
         create_logfile()
     load_all_model()
     root = Tk()
     root.title('Application Controller')
     root.geometry('250x300+0+0')
-
-    start_app = Button(root, text="START", width=20, bg='red', fg='white', command=run_app)
-    start_app.pack(padx=5, pady=5)
-    start_app = Button(root, text="Edit_polygon", width=20, bg='red', fg='white', command=run_config)
-    start_app.pack(padx=5, pady=5)
+    if auto_start == 'TRUE':
+        run_app()
+    elif auto_start == 'FALSE':
+        start_app = Button(root, text="START", width=20, bg='red', fg='white', command=run_app)
+        start_app.pack(padx=5, pady=5)
+    edit_poly = Button(root, text="Edit_polygon", width=20, bg='red', fg='white', command=run_config)
+    edit_poly.pack(padx=5, pady=5)
 
     admin = Button(root, text="ADMIN", width=20, bg='red', fg='white', command=admin_control)
     admin.pack(padx=5, pady=5, side="bottom")
